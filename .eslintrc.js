b2e737d84153fb0a72c601df543f20ee69dd0851
danielwerg <35052399+danielwerg@users.noreply.github.com>
2023-08-08 12:25:02 +0300

    chore: 👽️ Don't specify `parserOptions.project`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: '@danielwerg/eslint-config',
   root: true,
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.eslint.json'
-  }
+  parserOptions: { tsconfigRootDir: __dirname }
 };
