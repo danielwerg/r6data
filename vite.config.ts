eaaf0e57f6b60f3ee2fedf013872010d7c457d3c
danielwerg <35052399+danielwerg@users.noreply.github.com>
2023-08-08 15:02:36 +0300

    perf: ⚡️ Improve tree-shaking

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,37 @@ import svgo from 'vite-plugin-svgo';
 
 import { join } from 'node:path';
 
+import pkg from './package.json' assert { type: 'json' };
+
+const external = [
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/dot-notation
+  ...Object.keys(pkg['dependencies'] ?? {}),
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/dot-notation
+  ...Object.keys(pkg['peerDependencies'] ?? {})
+];
+
+/** Matches `^DEPENDENCY_NAME($|/)` */
+const makeExternalPredicate = (externalArr: string[]) =>
+  externalArr.length
+    ? (id: string) => new RegExp(`^(${externalArr.join('|')})($|/)`).test(id)
+    : () => false;
+
 export default defineConfig({
   build: {
     lib: {
       entry: join(__dirname, 'src/index.ts'),
-      name: 'r6data',
-      formats: ['es', 'cjs'],
       fileName: 'index'
+    },
+    rollupOptions: {
+      external: makeExternalPredicate(external),
+      output: [
+        { format: 'cjs' },
+        {
+          format: 'esm',
+          preserveModules: true,
+          entryFileNames: '[name].mjs'
+        }
+      ]
     }
   },
   plugins: [
